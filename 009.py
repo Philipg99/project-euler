6c38675792f5bb3d6cd25c220f7811b43695f2f1
Philipg99 <32796112+Philipg99@users.noreply.github.com>
2018-10-12 17:09:44 +0530

    Update 009.py

--- 009.py
+++ 009.py
@@ -1,10 +1,11 @@
 from time import time as t
-
+from math import ceil
 
 #straight forward 
 '''
 t1 = t()
 s = 1000
+
 for a in range(0,1000//3): 
     for b in range (a+1,1000//2):
         if a**2 + b**2 == (1000 - (a+b))**2:
@@ -16,7 +17,7 @@ for a in range(0,1000//3):
 
 #this one is like 1000 times faster
 t2 = t()
-from math import ceil
+
 for n in range(1, int( ceil(( (1000//2)**0.5 -1 )//2) ) ):
     for m in range(n,int(ceil((1000//2)**0.5))):
         if m*(m+n) == 1000//2:
@@ -26,4 +27,5 @@ for n in range(1, int( ceil(( (1000//2)**0.5 -1 )//2) ) ):
             print (a+b+c , a , b, c)
             print ('time =', (t() - t2)*10**5)
             exit(0)
+            
 #'''
