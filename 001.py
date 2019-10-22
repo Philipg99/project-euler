eddc3eb89da6a701004aa6a73a45e1019497fcaf
Philipg99 <32796112+Philipg99@users.noreply.github.com>
2019-10-22 14:15:09 +0530

    added new line
    for hactoberfest

--- 001.py
+++ 001.py
@@ -6,6 +6,7 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
+
 from time import time
 
 a = time()
